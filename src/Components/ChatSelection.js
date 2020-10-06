9fe12f37620e612da55f084a204560502d8cfb47
Kyle <kpete2017@gmail.com>
2020-10-05 22:47:59 -0600

    Added Chat Room Title

--- src/Components/ChatSelection.js
+++ src/Components/ChatSelection.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ChatSelection() {
     return(
         <div className="chat-selection-area">
-
+            <h1 className="chat-selection-title">Rooms</h1>
         </div>
     )
 }
